45eb8a2e8405a09efeb8880a46e6b429668d6a42
emocay3277 <chantel.lewis.kpop@gmail.com>
2018-07-09 09:32:58 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+ 
+
+var drawing = SVG('drawing-area');
+drawing.rect(100, 200)
+
+
+drawing.rect(100, 200).fill('purple')
+
+
+ 
+drawing.rect(100, 200)
+  .fill('purple')
+
+ 
+drawing.rect(100, 200)
+  .fill('purple')
+  .center(300, 300)
+
+ 
+drawing.ellipse(100, 300)
+  .stroke('green')
+  .center(200, 200)
+  .rotate(45)
+
+ 
+drawing.line(0, 0, 100, 200)
+  .stroke('yellow')
+
